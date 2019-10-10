d9e2298d035b0abd93ffba70cdebb1ab3baeda8e
igorcrexito <igorcrexito@gmail.com>
2019-10-10 12:50:03 -0300

    Update bubblenetwork.py

--- src/bubblenetwork.py
+++ src/bubblenetwork.py
@@ -17,13 +17,14 @@ import utils.utils as utils
 batch_size = 20
 clip_size = 4
 number_of_timesteps = 4
-appearance_representation_size = 4096
-number_of_classes = 51
-base_path = "D:/HMDB51/videos/"
+appearance_representation_size = 4096 #size of activations of FC7 from VGG16
+number_of_classes = 51 #51 classes from HMDB51
+base_path = "D:/HMDB51/videos/" #absolute paths to test the application
 protocol_path = "D:/HMDB51/splits/"
 number_of_epochs = 50
-split = 1
+split = 1 #this represents the split to be evaluated on the protocol file
 
+#just a main method to check model architecture
 if __name__ == "__mains__":
     #bubble_model = bm.create_bubble_model2(batch_size, number_of_timesteps, clip_size, appearance_representation_size, number_of_classes)
     bubble_model = bm.create_bubble_network(batch_size, 32, number_of_classes)
@@ -45,26 +46,30 @@ if __name__ == "__main__":
    #retrieving the list of activities
    list_of_activities = glob.glob("*")
    
+   #temporary number of classes to analyze partial outcomes
    new_classes = 6
    
    #for i in range(0, number_of_classes):
    for i in range(0, new_classes):
-        #reading optical flow videos
-        
+        #composing path to get videos
         video_path = base_path + str(list_of_activities[i]) + "/"
         print(video_path)
+        
+        #reading protocol file to place videos on test or train
         protocol_list = fr.read_protocol_path(protocol_path, split, str(list_of_activities[i]))
         
+        #reading optical flow videos and appearance activations
         flow_list_train, flow_list_test = fr.read_flow_videos(video_path, clip_size, number_of_timesteps, protocol_list)
         appearance_list_train, appearance_list_test = fr.read_appearance_files(video_path, clip_size, number_of_timesteps, protocol_list)
         
         #print(np.shape(appearance_list_train))
-        
+        #appending data to lists
         input_appearance_list_train.extend(appearance_list_train)
         input_appearance_list_test.extend(appearance_list_test)
         input_flow_list_train.extend(flow_list_train)
         input_flow_list_test.extend(flow_list_test)
         
+        #appending labels to output lists
         for z in range(0, len(flow_list_train)):
             output_label_list_train.append(i)
             
@@ -80,14 +85,12 @@ if __name__ == "__main__":
    
    #bubble_model = bm.create_bubble_model2(batch_size, number_of_timesteps, clip_size, appearance_representation_size, number_of_classes)
    
-   bubble_model = bm.create_bubble_network(batch_size, 16, new_classes)
-   output_label_list_train = keras.utils.to_categorical(output_label_list_train, num_classes=new_classes)
-   #for i in range(0, len(output_label_list_train)):
-   #    print(str(i) + ' ' + str(output_label_list_train[i]))
-   
+   bubble_model = bm.create_bubble_network(batch_size, 16, new_classes) #creating a model
+   output_label_list_train = keras.utils.to_categorical(output_label_list_train, num_classes=new_classes) #turning output labels into categorical values
+
+   #fitting model
    #bubble_model = bm.bubble_fit(bubble_model, input_flow_list_train, input_appearance_list_train, number_of_timesteps, clip_size, output_label_list_train, number_of_epochs, batch_size)
    bubble_model = bm.bubble_network_fit(bubble_model, input_flow_list_train, input_appearance_list_train, number_of_timesteps*clip_size, output_label_list_train, number_of_epochs, batch_size, new_classes)
-   
    
    input_flow_list_test = np.array([input_flow_list_test])
    input_appearance_list_test = np.array([input_appearance_list_test])
